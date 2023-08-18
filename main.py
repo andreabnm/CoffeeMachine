e7ed7577c0c2e11d090852c2d2396e92845a19c7
Andrea Bonomi <andreabnm@gmail.com>
2023-08-18 23:34:43 +0200

    Implementation

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ MENU = {
             "water": 200,
             "milk": 150,
             "coffee": 24,
+            "coffee": 24,
         },
         "cost": 2.5,
     },
@@ -30,4 +31,70 @@ resources = {
     "coffee": 100,
 }
 
-print(resources)
+isOn = True
+
+def PrintReport():
+    print(f'Water: {resources["water"]}ml')
+    print(f'Milk: {resources["milk"]}ml')
+    print(f'Coffee: {resources["coffee"]}g')
+    print(f'Money: ${round(resources["money"], 2)}')
+
+
+def CheckResources(drink):
+    ingredients = MENU[drink]["ingredients"]
+    for ingredient in ingredients:
+        if resources[ingredient] < ingredients[ingredient]:
+            print(f'Sorry there is not enough {ingredient}.')
+            return False
+    return True
+
+
+def CheckMoney(money, drink):
+    cost = MENU[drink]["cost"]
+    if money < cost:
+        print("Sorry that's not enough money. Money refunded.")
+        return False
+    return True
+
+
+def DeductIngredients(drink):
+    ingredients = MENU[drink]["ingredients"]
+    for ingredient in ingredients:
+        resources[ingredient] -= ingredients[ingredient]
+
+
+
+def InsertMoney():
+    print('Please insert coins.')
+    quarters = int(input('how many quarters?: '))
+    dimes = int(input('how many dimes?: '))
+    nickels = int(input('how many nickles?: '))
+    pennies = int(input('how many pennies?: '))
+    total = (pennies * 0.01) + (nickels * 0.05) +  (dimes * 0.10) +  (quarters * 0.25)
+
+    return total
+
+
+while isOn:
+    userChoice = input('What would you like? (espresso/latte/cappuccino): ')
+
+    if userChoice == 'off':
+        isOn = False
+    elif userChoice == 'report':
+        PrintReport()
+    else:
+        CheckResources(userChoice)
+        currMoney = InsertMoney()
+        if CheckMoney(currMoney, userChoice):
+            drinkCost = MENU[userChoice]["cost"]
+            currMoney -= drinkCost
+            if "money" in resources:
+                resources["money"] += drinkCost
+            else:
+                resources["money"] = drinkCost
+            if currMoney > 0:
+                print(f'Here is ${round(currMoney, 2)} dollars in change')
+            DeductIngredients(userChoice)
+            print(f'Here is your {userChoice}. Enjoy!')
+
+
