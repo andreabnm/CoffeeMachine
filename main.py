858617789efc4b86fd125d88a5b80e61348300f7
Andrea Bonomi <andreabnm@gmail.com>
2023-08-20 22:34:24 +0200

    changed function names

--- main.py
+++ main.py
@@ -33,14 +33,15 @@ resources = {
 
 isOn = True
 
-def PrintReport():
+
+def print_report():
     print(f'Water: {resources["water"]}ml')
     print(f'Milk: {resources["milk"]}ml')
     print(f'Coffee: {resources["coffee"]}g')
     print(f'Money: ${round(resources["money"], 2)}')
 
 
-def CheckResources(drink):
+def check_resources(drink):
     ingredients = MENU[drink]["ingredients"]
     for ingredient in ingredients:
         if resources[ingredient] < ingredients[ingredient]:
@@ -49,7 +50,7 @@ def CheckResources(drink):
     return True
 
 
-def CheckMoney(money, drink):
+def check_money(money, drink):
     cost = MENU[drink]["cost"]
     if money < cost:
         print("Sorry that's not enough money. Money refunded.")
@@ -57,20 +58,19 @@ def CheckMoney(money, drink):
     return True
 
 
-def DeductIngredients(drink):
+def deduct_ingredients(drink):
     ingredients = MENU[drink]["ingredients"]
     for ingredient in ingredients:
         resources[ingredient] -= ingredients[ingredient]
 
 
-
-def InsertMoney():
+def insert_money():
     print('Please insert coins.')
     quarters = int(input('how many quarters?: '))
     dimes = int(input('how many dimes?: '))
     nickels = int(input('how many nickles?: '))
     pennies = int(input('how many pennies?: '))
-    total = (pennies * 0.01) + (nickels * 0.05) +  (dimes * 0.10) +  (quarters * 0.25)
+    total = (pennies * 0.01) + (nickels * 0.05) + (dimes * 0.10) + (quarters * 0.25)
 
     return total
 
@@ -81,11 +81,11 @@ while isOn:
     if userChoice == 'off':
         isOn = False
     elif userChoice == 'report':
-        PrintReport()
+        print_report()
     else:
-        CheckResources(userChoice)
-        currMoney = InsertMoney()
-        if CheckMoney(currMoney, userChoice):
+        check_resources(userChoice)
+        currMoney = insert_money()
+        if check_money(currMoney, userChoice):
             drinkCost = MENU[userChoice]["cost"]
             currMoney -= drinkCost
             if "money" in resources:
@@ -94,7 +94,5 @@ while isOn:
                 resources["money"] = drinkCost
             if currMoney > 0:
                 print(f'Here is ${round(currMoney, 2)} dollars in change')
-            DeductIngredients(userChoice)
+            deduct_ingredients(userChoice)
             print(f'Here is your {userChoice}. Enjoy!')
-
-
